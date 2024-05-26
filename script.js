06b5a17ffd96b40e79c63260451ae2bfa2f4c964
sokolenko97 <90186669+sokolenko97@users.noreply.github.com>
2024-05-26 18:34:10 +0300

    +

--- script.js
+++ script.js
@@ -224,7 +224,7 @@ window.addEventListener("load", function () {
         const asideCartTag = document.querySelector('aside')
         if (asideCartTag) {
           const checkoutButton = document.querySelector('.cart__checkout-button')
-          if (checkoutButton) {
+          if (checkoutButton.classList.contains('cart__checkout-button')) {
             checkoutButton.replaceWith(checkoutButton.cloneNode(true))
             checkoutButton.classList.remove('cart__checkout-button')
           }
