90da300cdc20ba8d510f8d07832ae0e4910d6db8
sokolenko97 <90186669+sokolenko97@users.noreply.github.com>
2024-05-29 21:05:17 +0300

    +

--- script.js
+++ script.js
@@ -50,6 +50,9 @@ window.addEventListener("load", function () {
             // addCartToButton(productsData);
           }, 1000);
         }
+        else{
+          clearInterval(checkProductListInterval)
+        }
       }
 
       let checkProductListInterval
